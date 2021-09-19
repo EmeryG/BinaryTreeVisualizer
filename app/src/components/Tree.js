d19843ffd0ce9dd63121333b63cfceb3a664811f
EmeryG <Emerytgriffith@gmail.com>
2021-09-18 23:24:36 -0500

    Implemented flexbox CSS to create tree structure and automatically distribute binary nodes.

--- app/src/components/Tree.js
+++ app/src/components/Tree.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Line from './Line'
+import Line from './Line';
+import './Tree.css';
 
 export default class Tree extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { buttonMessage : "Start", line: (<div></div>)};
+        this.state = { buttonMessage : "Start", line: (<div style={{position: "absolute"}}></div>)};
 
         this.startAnimation = this.startAnimation.bind(this); 
         this.createLine = this.createLine.bind(this);
@@ -26,16 +27,20 @@ export default class Tree extends React.Component {
 
     render() {
         return (
-            <div>
-                <button id="Control" onClick={this.startAnimation}>
-                { this.state.buttonMessage }
-                </button>
-                <p>test</p>
-                <p>test2</p>
+            <div class="tree">
+                <div class="treerow">
+                    <button id="Control" onClick={this.startAnimation}>
+                        { this.state.buttonMessage }
+                    </button>
+                </div>
                 {this.state.line}
-                <button id="Test" onClick={this.createLine}>
-                    Create Line
-                </button> 
+                <div class="treerow">
+                    <p>test</p>
+                    <p>test2</p>
+                    <button id="Test" onClick={this.createLine}>
+                        Create Line
+                    </button> 
+                </div>
             </div>
         );
     }
